2724b0052337dc4dd87e11786e60986fab1583d5
IVÁN MUÑOZ SÁNCHEZ <ivamunsan4@alu.edu.gva.es>
2023-12-09 18:12:46 +0100

    VERSION 2 - Telegrama

--- GitHubTelegrama_IMS_2324/IMS_2324.Designer.cs
+++ GitHubTelegrama_IMS_2324/IMS_2324.Designer.cs
@@ -29,24 +29,91 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            label2 = new Label();
+            txtTelegrama = new TextBox();
+            label3 = new Label();
+            txtPrecio = new TextBox();
+            cbUrgente = new CheckBox();
+            button1 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(627, 401);
+            label1.Location = new Point(375, 248);
             label1.Name = "label1";
-            label1.Size = new Size(63, 15);
+            label1.Size = new Size(103, 30);
             label1.TabIndex = 0;
-            label1.Text = "VERSION 1";
+            label1.Text = "VERSION 2\r\nTelegrama Errores.";
             // 
-            // Form1
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(29, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(52, 21);
+            label2.TabIndex = 11;
+            label2.Text = "Texto";
+            // 
+            // txtTelegrama
+            // 
+            txtTelegrama.Location = new Point(29, 43);
+            txtTelegrama.Multiline = true;
+            txtTelegrama.Name = "txtTelegrama";
+            txtTelegrama.Size = new Size(449, 172);
+            txtTelegrama.TabIndex = 10;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(29, 264);
+            label3.Name = "label3";
+            label3.Size = new Size(41, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Coste:";
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(75, 256);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(160, 23);
+            txtPrecio.TabIndex = 8;
+            // 
+            // cbUrgente
+            // 
+            cbUrgente.AutoSize = true;
+            cbUrgente.Location = new Point(29, 221);
+            cbUrgente.Name = "cbUrgente";
+            cbUrgente.Size = new Size(73, 19);
+            cbUrgente.TabIndex = 7;
+            cbUrgente.Text = "Urgente?";
+            cbUrgente.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(241, 231);
+            button1.Name = "button1";
+            button1.Size = new Size(126, 48);
+            button1.TabIndex = 6;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // IMS_2324
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(512, 330);
+            Controls.Add(label2);
+            Controls.Add(txtTelegrama);
+            Controls.Add(label3);
+            Controls.Add(txtPrecio);
+            Controls.Add(cbUrgente);
+            Controls.Add(button1);
             Controls.Add(label1);
-            Name = "Form1";
+            Name = "IMS_2324";
             Text = "IMS_2324";
             ResumeLayout(false);
             PerformLayout();
@@ -55,5 +122,11 @@
         #endregion
 
         private Label label1;
+        private Label label2;
+        private TextBox txtTelegrama;
+        private Label label3;
+        private TextBox txtPrecio;
+        private CheckBox cbUrgente;
+        private Button button1;
     }
 }
--- GitHubTelegrama_IMS_2324/IMS_2324.cs
+++ GitHubTelegrama_IMS_2324/IMS_2324.cs
@@ -6,5 +6,36 @@ namespace GitHubTelegrama_IMS_2324
         {
             InitializeComponent();
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string textoTelegrama;
+            char tipoTelegrama = ' ';
+            int numPalabras = 0;
+            double coste;
+            //Leo el telegrama IMS_2324
+            textoTelegrama = txtTelegrama.Text;
+            // telegrama urgente? IMS_2324
+            if (cbUrgente.Checked)
+                tipoTelegrama = 'u';
+            //Obtengo el número de palabras que forma el telegrama IMS_2324
+            numPalabras = textoTelegrama.Length;
+            //Si el telegrama es ordinario IMS_2324
+            if (tipoTelegrama == 'o')
+                if (numPalabras <= 10)
+                    coste = 25;
+                else
+                    coste = 0.5 * numPalabras;
+            else
+            //Si el telegrama es urgente IMS_2324
+            if (tipoTelegrama == 'u')
+                if (numPalabras <= 10)
+                    coste = 5;
+                else
+                    coste = 5 + 0.75 * (numPalabras - 10);
+            else
+                coste = 0;
+            txtPrecio.Text = coste.ToString() + " euros";
+        }
     }
 }
