c09ed1382dffb4b3413168588e448d7792279806
IVÁN MUÑOZ SÁNCHEZ <ivamunsan4@alu.edu.gva.es>
2023-12-09 17:41:04 +0100

    VERSION 1

--- GitHubTelegrama_IMS_2324/Form1.Designer.cs
+++ GitHubTelegrama_IMS_2324/Form1.Designer.cs
@@ -28,18 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(627, 401);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 15);
+            label1.TabIndex = 0;
+            label1.Text = "VERSION 1";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "IMS_2324";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
