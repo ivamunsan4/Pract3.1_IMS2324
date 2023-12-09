ef5feaff7c4b80a94e19becd02dfd58799be08a1
IVÁN MUÑOZ SÁNCHEZ <ivamunsan4@alu.edu.gva.es>
2023-12-09 19:57:22 +0100

    VERSION 2 - TELEGRAMA ERRORES

--- GitHubTelegrama_IMS_2324/IMS_2324.Designer.cs
+++ GitHubTelegrama_IMS_2324/IMS_2324.Designer.cs
@@ -42,9 +42,9 @@
             label1.AutoSize = true;
             label1.Location = new Point(375, 248);
             label1.Name = "label1";
-            label1.Size = new Size(103, 30);
+            label1.Size = new Size(106, 30);
             label1.TabIndex = 0;
-            label1.Text = "VERSION 2\r\nTelegrama Errores.";
+            label1.Text = "VERSION 2\r\nTelegrama Errores..";
             // 
             // label2
             // 
