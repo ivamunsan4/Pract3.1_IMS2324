d78f4b0f4d2c04a7bc835e764e44ab8567495a88
IVÁN MUÑOZ SÁNCHEZ <ivanms@outlook.com>
2023-12-09 17:47:18 +0100

    VERSION 2

--- GitHubTelegrama_IMS_2324/Form1.Designer.cs
+++ GitHubTelegrama_IMS_2324/Form1.Designer.cs
@@ -38,7 +38,7 @@
             label1.Name = "label1";
             label1.Size = new Size(63, 15);
             label1.TabIndex = 0;
-            label1.Text = "VERSION 1";
+            label1.Text = "VERSION 2";
             // 
             // Form1
             // 
